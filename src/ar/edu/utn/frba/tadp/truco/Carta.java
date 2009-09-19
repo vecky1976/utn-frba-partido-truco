580884c45b5e475a29db672e24a2d8e5bc3563e7
pabloramos1987@gmail.com <pabloramos1987@gmail.com@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-19 21:07:12 +0000

    Borre los sources de las interfases que nos paso la catedra que se habian creado por error.

--- src/ar/edu/utn/frba/tadp/truco/Carta.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frba.tadp.truco;
-
-public interface Carta {
-	
-	public int getNumero();
-	public Palo getPalo();
-	
-}
--- src/ar/edu/utn/frba/tadp/truco/Palo.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.utn.frba.tadp.truco;
-
-public enum Palo {
-	ESPADA, BASTO, ORO, COPA 
-}
