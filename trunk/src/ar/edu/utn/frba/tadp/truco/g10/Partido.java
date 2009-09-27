a1b9f9c18eaabb363aa4482be864dac41249b43c
demian.gonda <demian.gonda@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-27 23:58:18 +0000

    Se implementan los metodos necesarios para prueba de "se fue al mazo". Se agrega nota de diseño sobre patron Facade.

--- trunk/src/ar/edu/utn/frba/tadp/truco/g10/Partido.java
+++ trunk/src/ar/edu/utn/frba/tadp/truco/g10/Partido.java
@@ -1,20 +1,62 @@
 package ar.edu.utn.frba.tadp.truco.g10;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Clase Partido
+ * 
+ * Implementa una Facade entre el jugador y el Sistema de Truco.
+ * 
+ */
 public class Partido {
+	
+	//TODO - Revisar si en este caso es correcto usar Facade. La idea sería que el jugador solamente conosca al Partido.
+	
 	private Jugador jugador1;
 	private Jugador jugador2;
 	private Mazo mazo;
+	private ArrayList<Ronda> rondasDelPartido = new ArrayList<Ronda>();
 
-	public Partido(Jugador j1, Jugador j2, Mazo mazo) {
+	public Partido(Jugador j1, Jugador j2, Mazo mazo) {		
 		this.jugador1 = j1;
-		this.jugador2 = j2;
-		this.mazo = mazo;
+		j1.entrarEnElPartido(this);
 		
+		this.jugador2 = j2;
+		j2.entrarEnElPartido(this);
+		
+		this.mazo = mazo;
 	}
 
 	public void repartirCartas() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public boolean isFinDeRonda() {
+		return getUltimaRonda().isTerminada();
+	}
+
+	private Ronda getUltimaRonda() {
+		return rondasDelPartido.get(rondasDelPartido.size()-1);
+	}
+
+	public void comenzarPartido() {
+		Ronda rondaInicial = new Ronda();
+		rondasDelPartido.add(rondaInicial);
+	}
+
+	public void devolverCartas(Collection<CartaDeTruco> mano) {
+		mazo.devolverCartas(mano);		
+	}
+
+	public void seVaAlMazo(Collection<CartaDeTruco> manoDelJugador,Jugador jugador) {
+		mazo.devolverCartas(manoDelJugador);
+		getUltimaRonda().seVaAlMazo(jugador);
+	}
+
+	public boolean isSeFueAlMazo(Jugador jugador) {
+		return getUltimaRonda().seFueAlMazo(jugador);
 	}
 
 }
