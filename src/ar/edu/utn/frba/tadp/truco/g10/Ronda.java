c75d07e89448649010bb3215327802c8cd2e37dd
demian.gonda <demian.gonda@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-27 23:55:03 +0000

    Se implementa la clase Ronda. Un partido puede tener 1 a n cantidad de rondas.

---
+++ src/ar/edu/utn/frba/tadp/truco/g10/Ronda.java
@@ -0,0 +1,37 @@
+package ar.edu.utn.frba.tadp.truco.g10;
+
+import java.util.ArrayList;
+
+public class Ronda {
+	 
+	private ArrayList<Jugada> jugadasDeLaRonda = new ArrayList<Jugada>();
+	private boolean rondaEnCurso;
+	
+	public Ronda(){
+		rondaEnCurso = true;
+		Jugada primeraJugada = new Jugada();
+		jugadasDeLaRonda.add(primeraJugada);
+	}
+	
+	public boolean isTerminada(){
+		return !rondaEnCurso;
+	}
+
+	public void seVaAlMazo(Jugador jugador) {
+		getUltimaJugada().seVaAlMazo(jugador);
+		terminarRonda();
+	}
+
+	private void terminarRonda() {
+		this.rondaEnCurso = false;
+	}
+
+	private Jugada getUltimaJugada() {
+		return jugadasDeLaRonda.get(jugadasDeLaRonda.size()-1);
+	}
+
+	public boolean seFueAlMazo(Jugador jugador) {
+		return getUltimaJugada().seFueAlMazo(jugador);
+	}
+	
+}
