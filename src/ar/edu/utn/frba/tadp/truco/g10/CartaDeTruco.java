1c21497bc31249d92bc63d22cd971e570fa1029e
matias.tokar <matias.tokar@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-27 01:29:17 +0000

    modificacion de CartaDeTruco

--- src/ar/edu/utn/frba/tadp/truco/g10/CartaDeTruco.java
+++ src/ar/edu/utn/frba/tadp/truco/g10/CartaDeTruco.java
@@ -1,9 +1,35 @@
 package ar.edu.utn.frba.tadp.truco.g10;
 
+import ar.edu.utn.frba.tadp.truco.Carta;
 import ar.edu.utn.frba.tadp.truco.Palo;
 
-public class CartaDeTruco {
+public class CartaDeTruco implements Carta {
+	
 	private Palo palo;
-	private int valor;
+	private int numero;
 
+	public CartaDeTruco (Palo unPalo, int unNumero){
+		this.setNumero(numero);
+		this.setPalo(palo);
+	}
+	
+	@Override
+	public Palo getPalo() {
+		// TODO Auto-generated method stub
+		return this.palo;
+	}
+
+	public void setPalo(Palo palo) {
+		this.palo = palo;
+	}
+	
+	@Override
+	public int getNumero() {
+		// TODO Auto-generated method stub
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 }
