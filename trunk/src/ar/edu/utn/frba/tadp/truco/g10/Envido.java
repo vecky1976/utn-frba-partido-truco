9407c3c3d951dc2caf494101dc7e52a8ed1a93bd
matias.tokar <matias.tokar@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-28 01:28:29 +0000

    update test envido

--- trunk/src/ar/edu/utn/frba/tadp/truco/g10/Envido.java
+++ trunk/src/ar/edu/utn/frba/tadp/truco/g10/Envido.java
@@ -1,6 +1,9 @@
 package ar.edu.utn.frba.tadp.truco.g10;
 
 import java.util.ArrayList;
+import java.util.Collection;
+
+import ar.edu.utn.frba.tadp.truco.Carta;
 
 /**
  * Abstrae las operaciones que realiza el Envido en un juego de truco
@@ -13,15 +16,32 @@ public class Envido {
 		
 	}
 	
-	// verificar si dos cartas de tres son del mismo palo
-	public boolean tieneEnvido(ArrayList<CartaDeTruco> tresCartas){
-		if ( (tresCartas.get(0).getPalo() == tresCartas.get(1).getPalo()) ||
-				(tresCartas.get(0).getPalo() == tresCartas.get(2).getPalo()) ||
-				(tresCartas.get(1).getPalo() == tresCartas.get(2).getPalo())
+	// verificar si al menos dos cartas de tres son del mismo palo
+	public boolean tieneEnvido(Collection<Carta> cartasDeUnaMano){
+		Collection<Carta> cartasOro = new ArrayList<Carta>();
+		Collection<Carta> cartasCopa = new ArrayList<Carta>();
+		Collection<Carta> cartasEspada = new ArrayList<Carta>();
+		Collection<Carta> cartasBasto = new ArrayList<Carta>();
+		// filtrar sub colecciones por mismo palo
+		for (Carta unaCarta : cartasDeUnaMano){
+			switch (unaCarta.getPalo())
+			{
+			case ORO: cartasOro.add(unaCarta); break;
+			case ESPADA: cartasEspada.add(unaCarta); break;
+			case BASTO: cartasBasto.add(unaCarta); break;
+			case COPA: cartasCopa.add(unaCarta); break;
+			}
+		}
+		// si cualquier coleccion del mismo palo tiene
+		// dos o mas cartas tiene envido
+		if (	(cartasOro.size()> 1) ||
+				(cartasEspada.size()> 1) ||
+				(cartasBasto.size()> 1) ||
+				(cartasCopa.size()> 1)
 			)
-				return true;
-		
-		return false;
+			return true;
+		else
+			return false;
 	}
 	
 	/**
@@ -32,8 +52,8 @@ public class Envido {
 	 * @param otrasTresCartas
 	 * @return
 	 */
-	public boolean ganaEnvido(	ArrayList<CartaDeTruco> tresCartas,
-								ArrayList<CartaDeTruco> otrasTresCartas ){
+	public boolean ganaEnvido(	Collection<Carta> tresCartas,
+								Collection<Carta> otrasTresCartas ){
 		return this.getValorEnvido(tresCartas) > this.getValorEnvido(otrasTresCartas);
 		
 	}
@@ -41,20 +61,20 @@ public class Envido {
 	// deberia pasarle 3 cartas y que considere:
 	// - si no tiene 2 del mismo palo el valor envido es la carta de mayor numero
 	// - si tiene envido sumo las 2 mayores del mismo palo
-	public int getValorEnvido(ArrayList<CartaDeTruco> tresCartas) {
-		ArrayList<CartaDeTruco> mayoresCartas = new ArrayList<CartaDeTruco>();
+	public int getValorEnvido(Collection<Carta> cartasDeUnaMano) {
+		Collection<Carta> mayoresCartas = new ArrayList<Carta>();
 		
-		if (this.tieneEnvido(tresCartas)){
-			mayoresCartas = this.getDosMayoresMismoPalo(tresCartas);
-			return this.calcularEnvido(mayoresCartas.get(0), mayoresCartas.get(1));
+		if (this.tieneEnvido(cartasDeUnaMano)){
+			mayoresCartas = this.getDosMayoresMismoPalo(cartasDeUnaMano);
+			return this.calcularEnvido(((ArrayList<Carta>) mayoresCartas).get(0), ((ArrayList<Carta>) mayoresCartas).get(1));
 		}
-		return this.getMayorValorCarta(tresCartas);
+		return this.getMayorValorCarta(cartasDeUnaMano);
 	}
 	
-	private int getMayorValorCarta(ArrayList<CartaDeTruco> tresCartas) {
+	private int getMayorValorCarta(Collection<Carta> tresCartas) {
 		int mayorValorEnvido = -1;
 		// TODO Auto-generated method stub
-		for (CartaDeTruco carta : tresCartas) {
+		for (Carta carta : tresCartas) {
 			if (this.getValorEnvidoCarta(carta.getNumero()) > mayorValorEnvido){
 				mayorValorEnvido = this.getValorEnvidoCarta(carta.getNumero());
 			}
@@ -62,14 +82,66 @@ public class Envido {
 		return mayorValorEnvido;
 	}
 
-	private ArrayList<CartaDeTruco> getDosMayoresMismoPalo(
-			ArrayList<CartaDeTruco> tresCartas) {
+	private Collection<Carta> getDosMayoresMismoPalo(
+			Collection<Carta> tresCartas) {
 		// TODO Auto-generated method stub
-		ArrayList<CartaDeTruco> cartasEnvido = new ArrayList<CartaDeTruco>();
+		Collection<Carta> cartasOro = new ArrayList<Carta>();
+		Collection<Carta> cartasCopa = new ArrayList<Carta>();
+		Collection<Carta> cartasEspada = new ArrayList<Carta>();
+		Collection<Carta> cartasBasto = new ArrayList<Carta>();
 		// filtrar sub coleccion por mismo palo
-		// si son 2 elementos > retorno esos
-		// si son 3 del mismo palo > retorno las 2 de mayorValorEnvido
-		return null;
+		for (Carta unaCarta : tresCartas){
+			switch (unaCarta.getPalo())
+			{
+			case ORO: cartasOro.add(unaCarta);
+			case ESPADA: cartasEspada.add(unaCarta);
+			case BASTO: cartasBasto.add(unaCarta);
+			case COPA: cartasCopa.add(unaCarta);
+			}
+		}
+		
+		// si son 2 elementos -> retorno esos
+		if (cartasOro.size()==2)
+			return cartasOro;
+		if (cartasCopa.size()==2)
+			return cartasCopa;
+		if (cartasBasto.size()==2)
+			return cartasBasto;
+		if (cartasEspada.size()==2)
+			return cartasEspada;
+		
+		// si son 3 del mismo palo -> retorno las 2 de mayorValorEnvido
+		if (cartasOro.size()==3) {
+			// no va a funcionar por estoy sacando otro objeto que no esta en la coleccion
+			cartasOro.remove(this.getCartaMenorValorEnvido(cartasOro));
+			return cartasOro;
+		}
+		if (cartasCopa.size()==3){
+			cartasCopa.remove(this.getCartaMenorValorEnvido(cartasCopa));
+			return cartasCopa;
+		}
+		if (cartasBasto.size()==3){
+			cartasBasto.remove(this.getCartaMenorValorEnvido(cartasBasto));
+			return cartasBasto;
+		}
+		if (cartasEspada.size()==3){
+			cartasEspada.remove(this.getCartaMenorValorEnvido(cartasEspada));
+			return cartasEspada;
+		}
+		return null;// no deveria llegar aca, excepcion
+	}
+
+	private Carta getCartaMenorValorEnvido(Collection<Carta> cartasDeUnaMano) {
+		Carta unaCarta = null;
+		int menorValorEnvido = 20; // no existe este valor de carta de envido
+		// TODO Auto-generated method stub
+		for (Carta carta : cartasDeUnaMano) {
+			if (this.getValorEnvidoCarta(carta.getNumero()) < menorValorEnvido){
+				menorValorEnvido = this.getValorEnvidoCarta(carta.getNumero());
+				unaCarta = new CartaDeTruco(carta.getPalo(), carta.getNumero());
+			}
+		}
+		return unaCarta;
 	}
 
 	// si es figura vale cero, entro caso el numero de la carta
@@ -85,8 +157,9 @@ public class Envido {
 		}
 	}
 	
-	public int calcularEnvido(CartaDeTruco unaCarta, CartaDeTruco otraCarta){
+	public int calcularEnvido(Carta unaCarta, Carta otraCarta){
 		return this.getValorEnvidoCarta(unaCarta.getNumero()) + 
 				this.getValorEnvidoCarta(otraCarta.getNumero()) + 20;
 	}
+
 }
--- trunk/test/ar/edu/utn/frba/tadp/truco/TestEnvido.java
+++ trunk/test/ar/edu/utn/frba/tadp/truco/TestEnvido.java
@@ -3,6 +3,9 @@ package ar.edu.utn.frba.tadp.truco;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,28 +13,87 @@ import ar.edu.utn.frba.tadp.truco.g10.CartaDeTruco;
 import ar.edu.utn.frba.tadp.truco.g10.Envido;
 
 public class TestEnvido {
-	private Palo BASTO;
-	private CartaDeTruco c1, c2, c3, c4;
+	private CartaDeTruco c1, c2, c3;
 	private Envido envido;
+	private Collection<Carta> tresCartas;
 
 	@Before
 	public void setUp() throws Exception {
-		envido = new Envido();
-		c3 = new CartaDeTruco(BASTO, 6);
-		c4 = new CartaDeTruco(BASTO, 7);
+		this.envido = new Envido();
 	}
 
 	@Test
 	public void testTieneEnvidoDeFiguras(){
 		// crear tres cartas con envido = 20 (2 figuras)
-		c1 = new CartaDeTruco(BASTO, 10);
-		c2 = new CartaDeTruco(BASTO, 11);
+		c1 = new CartaDeTruco(Palo.BASTO, 10);
+		c2 = new CartaDeTruco(Palo.BASTO, 11);
 		
 		assertTrue(envido.calcularEnvido(c1, c2)==20);
 	}
 	
 	@Test
 	public void testTieneMaximoEnvido(){
-		assertTrue(envido.calcularEnvido(c3, c4)==33);
+		c1 = new CartaDeTruco(Palo.BASTO, 6);
+		c2 = new CartaDeTruco(Palo.BASTO, 7);
+		assertTrue(envido.calcularEnvido(c1, c2)==33);
 	}
+	
+	@Test
+	public void testTieneEnvidoDeFiguraConNumero(){
+		c1 = new CartaDeTruco(Palo.BASTO, 12);
+		c2 = new CartaDeTruco(Palo.BASTO, 5);
+		assertTrue(envido.calcularEnvido(c1, c2)==25);
+	}
+	
+	// tiene envido si tiene al menos dos cartas del mismo palo
+	@Test
+	public void testTieneEnvidoConTresCartasMismoPalo(){
+
+		this.tresCartas = new ArrayList<Carta>();
+		
+		this.c1 = new CartaDeTruco(Palo.BASTO, 12);
+		this.c2 = new CartaDeTruco(Palo.BASTO, 5);
+		this.c3 = new CartaDeTruco(Palo.BASTO, 4);
+		
+		this.tresCartas.add(c1);
+		this.tresCartas.add(c2);
+		this.tresCartas.add(c3);
+		
+		assertTrue(envido.tieneEnvido(this.tresCartas));
+	}
+	
+	// no tiene envido si tiene tres cartas de distinto palo
+	@Test
+	public void testNoTieneEnvido(){
+		
+		this.tresCartas = new ArrayList<Carta>();
+		
+		this.c1 = new CartaDeTruco(Palo.BASTO, 12);
+		this.c2 = new CartaDeTruco(Palo.ESPADA, 5);
+		this.c3 = new CartaDeTruco(Palo.ORO, 4);
+		
+		this.tresCartas.add(c1);
+		this.tresCartas.add(c2);
+		this.tresCartas.add(c3);
+		
+		assertFalse(envido.tieneEnvido(this.tresCartas));
+	}
+	
+	// tiene dos posibles valores envido pero se canta el mayor
+	@Test
+	public void testMayorValorEnvidoConTresCartasMismoPalo(){
+
+		this.tresCartas = new ArrayList<Carta>();
+		
+		this.c1 = new CartaDeTruco(Palo.BASTO, 12);
+		this.c2 = new CartaDeTruco(Palo.BASTO, 5);
+		this.c3 = new CartaDeTruco(Palo.BASTO, 4);
+		
+		this.tresCartas.add(c1);
+		this.tresCartas.add(c2);
+		this.tresCartas.add(c3);
+		
+		assertTrue(envido.getValorEnvido(this.tresCartas)==29);
+	}
+	
 }
