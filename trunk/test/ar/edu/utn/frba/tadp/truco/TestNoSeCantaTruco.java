8078a7cab15817bb827a48a1672a1e3fbd1c4900
pabloramos1987@gmail.com <pabloramos1987@gmail.com@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-29 20:05:30 +0000

    Se generaron unas pruebas para poder realizar el test de NoSeCantoTruco.

--- trunk/test/ar/edu/utn/frba/tadp/truco/TestNoSeCantaTruco.java
+++ trunk/test/ar/edu/utn/frba/tadp/truco/TestNoSeCantaTruco.java
@@ -1,5 +1,7 @@
 package ar.edu.utn.frba.tadp.truco;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -7,34 +9,46 @@ import org.junit.Test;
 import ar.edu.utn.frba.tadp.truco.g10.Jugador;
 import ar.edu.utn.frba.tadp.truco.g10.Mano;
 import ar.edu.utn.frba.tadp.truco.g10.Mazo;
-import ar.edu.utn.frba.tadp.truco.g10.Partido;
 
 public class TestNoSeCantaTruco {
-	
+
 	Jugador jugador1;
 	Jugador jugador2;
 	Mazo mazo;
-	Mano mano ;
-	Partido partido;
-	
+	Mano mano;
 
 	@Before
 	public void setUp() throws Exception {
 		jugador1 = new Jugador("Pepe");
-		jugador2= new Jugador ("Tito");
+		jugador2 = new Jugador("Tito");
 		mazo = new Mazo();
-		mano = new Mano(jugador1,jugador2,mazo);
-		partido  = new Partido(jugador1, jugador1, mazo);
-		
-		
-	}
-	
-	@Test
-	public void noSeCantoTruco(){
-		mano.prepararMano();
-//		mano.iniciar
+		mano = new Mano(jugador1, jugador2, mazo);
 
-				
+	}
+
+	@Test
+	public void noSeCantoTruco() {
+		
+		assertFalse(mano.alguienCantoTruco());
+
+	}
+
+	@Test
+	public void seCantoTruco() {
+		mano.seCantoTruco();
+		assertTrue(mano.alguienCantoTruco());
+	}
+
+	@Test
+	public void cantoTrucoLaMano() {
+		jugador1.truco();
+		assertTrue(jugador1.canteTruco());
+	}
+
+	@Test
+	public void cantoTrucoElPie() {
+		jugador2.truco();
+		assertTrue(jugador2.canteTruco());
 	}
 
 }
