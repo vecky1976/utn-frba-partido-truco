5b3f75d617a8e178bba5c4fe591262171b8940c1
pabloramos1987@gmail.com <pabloramos1987@gmail.com@c11337c4-9d3e-11de-a3fa-a586d8720ea5>
2009-09-29 15:29:43 +0000

    Se craron nuevos metodos en Mano y Rondas para poder realizar las jugadas y completar los test de NoSeCantoTruco

--- trunk/src/ar/edu/utn/frba/tadp/truco/g10/Mano.java
+++ trunk/src/ar/edu/utn/frba/tadp/truco/g10/Mano.java
@@ -1,40 +1,49 @@
 package ar.edu.utn.frba.tadp.truco.g10;
 
+import java.util.ArrayList;
+
 public class Mano {
-	
+
 	public Mano(Jugador jugador1, Jugador jugador2, Mazo mazo) {
 		this.jugadorMano = jugador1;
 		this.jugadorResto = jugador2;
 		this.mazo = mazo;
+		this.rondasDeLaMano = new ArrayList<Ronda>();
+
 	}
-		
+
 	private Jugador jugadorMano;
 	private Jugador jugadorResto;
 	private Mazo mazo;
-	
+	private ArrayList<Ronda> rondasDeLaMano;
+
 	public void jugarRondas() {
-		// TODO Auto-generated method stub
-		
+		Ronda rondaEnCurso = new Ronda();
+		while (this.rondasDeLaMano.size() <= 3 && this.manoEnCurso) {
+			rondaEnCurso.jugar();
+			this.rondasDeLaMano.add(rondaEnCurso);
+
+		}
+
 	}
-	/** Se prepara todo para que se puedan iniciar las rondas de la mano.
-	 * Se reparten 3 cartas a cada jugador.
+	/**
+	 * Se prepara todo para que se puedan iniciar las rondas de la mano. Se
+	 * reparten 3 cartas a cada jugador.
 	 * 
 	 */
-	public void prepararMano(){
+	public void prepararMano() {
 		this.repartirCartas();
 	}
 
-	/** A cada uno de los jugadores de la mano se le reparten tres cartas.
+	/**
+	 * A cada uno de los jugadores de la mano se le reparten tres cartas.
 	 * 
 	 */
 	private void repartirCartas() {
 		// TODO Auto-generated method stub
 		this.jugadorMano.aceptarCartas(this.mazo.darTresCartas());
 		this.jugadorResto.aceptarCartas(this.mazo.darTresCartas());
-		
-		
+
 	}
-	
-	
 
 }
